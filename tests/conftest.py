e9353192344b76d19017471c0f1cb6755e064c30
Igor Degtyarenko <div50015@mail.ru>
2024-03-24 09:35:03 +0300

    Edit conftest.py file

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,44 +4,23 @@ import allure_commons
 from appium.options.android import UiAutomator2Options
 from selene import browser, support
 import os
-from dotenv import load_dotenv
 import config
 import utils
-
 from appium import webdriver
-
-
-# @pytest.fixture(scope='session', autouse=True)
-# def load_env():
-#     load_dotenv()
 
 
 @pytest.fixture(scope='function', autouse=True)
 def mobile_management():
-    # login = os.getenv('LOGIN')
-    # password = os.getenv('PASSWORD')
     options = UiAutomator2Options().load_capabilities({
-        # Specify device and os_version for testing
-        # "platformName": "android",
         "platformVersion": "9.0",
         "deviceName": "Google Pixel 3",
-
-        # Set URL of the application under test
         "app": "bs://sample.app",
-
-        # Set other BrowserStack capabilities
         'bstack:options': {
             "projectName": "Mobile Tests Lesson 21",
             "buildName": "browserstack-build-1",
             "sessionName": "BStack android_test",
-
-            # Set your access credentials
             "userName": f'{config.login}',
             "accessKey": f'{config.password}'
-            # "userName": "bsuser_gEb24K",
-            # "accessKey": "y2txdesxJgRFezzqc67Y"
-            # "userName": "iakivkramarenko_qKHOLN",
-            # "accessKey": "FSHAmndKHW3XsDkgm5zT"
         }
     })
 
